ee316186605cbafdd9aac5d7dc014830f2648797
zhoubo <bo.zhou@ruhlamat-st.com.cn>
2025-06-18 15:38:12 +0800

    调整脚本
    根据现场微调

---
+++ mongodb/linux_part_collection_mongodump_copy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# MongoDB连接信息
+mongo_host="127.0.0.1"      # MongoDB主机地址
+mongo_port="27017"            # MongoDB端口号
+mongo_db="emgda"              # 数据库名称
+mongo_username="emgda"        # MongoDB用户名
+mongo_password="ShopWorx110T" # MongoDB密码
+
+elementName=(
+    "process_substation-381" "process_substation-384" "rework" "process_substation-360" "checkin"
+    "process_substation-371" "process_substation-358" "process_substation-367" "process_substation-366"
+    "process_substation-372" "process_substation-382" "process_substation-383" "process_substation-353"
+    "process_substation-364" "process_substation-351" "process_substation-368" "process_substation-369"
+    "process_substation-354" "process_substation-355"
+)
+
+# 执行MongoDB查询并将输出保存到变量
+ # 循环遍历group_output中的值
+for value in "${elementName[@]}"; do
+    # 在循环中处理每个值，你可以添加其他逻辑处理
+    echo "Processing value: $value"
+    # 使用mongodump数据
+    backup_folder="/opt/collection/${value}"
+    mongodump --gzip --host $mongo_host --port $mongo_port --username $mongo_username --password $mongo_password --db $mongo_db --collection default --query "{ 'elementName': '$value' }" --out "$backup_folder"
+done
